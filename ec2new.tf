d68bb0b363904202fb1873cdb6a2d2d7865d6e46
hydave25 <56361829+hydave25@users.noreply.github.com>
2021-08-07 17:22:26 +0530

    Update ec2new.tf

--- ec2new.tf
+++ ec2new.tf
@@ -2,5 +2,5 @@ resource "aws_instance" "myec2" {
    ami = "ami-09e67e426f25ce0d7"
    instance_type = "t2.small"
 #   key_name = "ec2-key"
-   vpc_security_group_ids  = [aws_security_group.allow_ssh_conn.id]
+#   vpc_security_group_ids  = [aws_security_group.allow_ssh_conn.id]
 }
